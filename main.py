206b07d1e3181ef7945bd77306cbe7caddda6850
Mitsuo10 <pedro.risardi@gmail.com>
2026-09-02 21:30:41 -0300

    feat: criar sequencia de cadeias

--- main.py
+++ main.py
@@ -14,14 +14,32 @@ class Destino(BaseModel):
     cidade:str = Field("A cidade recomendada para visitar")
     motivo:str = Field("Motivo pelo qual é interessante visitar essa cidade")
 
-parseador = JsonOutputParser(pydantic_object = Destino)
+class Restaurantes(BaseModel):
+    cidade:str = Field("A cidade recomendada para visitar")
+    motivo:str = Field("Motivo pelo qual é interessante visitar essa cidade")
+
+parseador_destino = JsonOutputParser(pydantic_object = Destino)
+parseador_restaurante = JsonOutputParser(pydantic_object = Restaurantes)
 
 prompt_cidade = PromptTemplate(
         template="""
     Sugira uma cidade dado o meu interesse por {interesse}.
+    {formato_de_saida}
     """,
     input_variables=["interesse"],
-    partial_variables={"formato_de_saida": parseador.get_format_instructions()}
+    partial_variables={"formato_de_saida": parseador_destino.get_format_instructions()}
+)
+
+prompt_restaurante = PromptTemplate(
+        template="""
+    Sugira restaurantes populares entre locais em {cidade}.
+    {formato_de_saida}
+    """,
+    partial_variables={"formato_de_saida": parseador_restaurante.get_format_instructions()}
+)
+
+prompt_cultural = PromptTemplate(
+    template="Sugira atividades e locais culturais em {cidade}"
 )
 
 modelo = ChatOpenAI(
@@ -30,7 +48,11 @@ api_key=api_key,
 base_url="https://api.groq.com/openai/v1"
 )
 
-cadeia = prompt_cidade | modelo | StrOutputParser()
+cadeia1 = prompt_cidade | modelo | parseador_destino
+cadeia2 = prompt_restaurante | modelo | parseador_restaurante
+cadeia3 = prompt_cultural | modelo | StrOutputParser()
+
+cadeia = (cadeia1 | cadeia2 | cadeia3)
 
 resposta = cadeia.invoke({
     "interesse" : "praias"
